57cd2bc6af5bbc37db68847f2c0ba1f2624916ba
alexdembele <chessdembel@gmail.com>
2024-02-01 10:53:33 +0100

    td2

--- src/cpuTest.cpp
+++ src/cpuTest.cpp
@@ -1,28 +1,105 @@
 #include <cstring>
 #include <iostream>
 #include <cstdlib>
-void incr(unsigned int nLoops, double* pCounter)
+#include <time.h>
+#include <string>
+#include "../inc/timespec.hpp"
+struct Data 
 {
-    for(unsigned int u=0; u< nLoops; u++)
+    
+    volatile double  counter;
+    unsigned long nLoops;
+    bool protection;
+    pthread_mutex_t mutex;
+};
+
+
+void incr(unsigned long nLoops, double* pCounter,pthread_mutex_t* mutex, bool protection)
+{
+    if(protection)
     {
-        *pCounter+=1;
+        for(unsigned int u=0; u< nLoops; u++)
+        {
+            pthread_mutex_lock(mutex);
+            *pCounter+=1;
+            pthread_mutex_unlock(mutex);
+        }
     }
+    else
+    {
+        for(unsigned int u=0; u< nLoops; u++)
+        {
+            *pCounter+=1;
+        }
+    }
+
 }
+void* call_incr(void* v_data)
+{
+    Data* p_data = (Data*) v_data;
+
+    incr(p_data->nLoops, (double *)&p_data->counter,&p_data->mutex,p_data->protection);
+    return v_data;
+}
+
+
+
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <nombre_entier>" << std::endl;
+    bool protec;
+    if (argc != 4 && argc !=3) 
+    {
+        std::cerr << "Usage: " << argv[0] << " <nombre_entier> <nombre de tâche> (<protected>)" << std::endl;
         return 1;
     }
-
-    // Convertir l'argument en entier
-    unsigned int nLoops = (unsigned int)std::atoi(argv[1]);
+    if(argc== 4)
+    {
+        if(std::strcmp(argv[3], "protected") == 0)
+        {
+            std::cout << "Fonctionnement avec Mutex" << std::endl;
+            protec=true;
+        }
+        else
+        {
+            std::cout << "Fonctionnement sans Mutex" << std::endl;
+            protec=false;
+        }
+    }
+    else
+    {
+        std::cout << "Fonctionnement sans Mutex" << std::endl;
+        protec=false;
+    }
     
-    double counter = 0;
+    
+    unsigned long nLoops = (unsigned long)std::atoi(argv[1]);
+    unsigned int nTasks = (unsigned int)std::atoi(argv[2]);
 
+    Data data = {  0.0 , nLoops, protec};
+    pthread_mutex_init(&data.mutex, nullptr);
+    pthread_t incrementThread[nTasks];
+    for(int i=0;i<nTasks;i++)
+    {
+        pthread_create(&incrementThread[i], nullptr, call_incr, &data);
+    }
+    
+    /*timespec a = timespec_now();
     incr(nLoops, &counter);
-    std::cout << "Valeur finale du counter : " << counter << std::endl;
+    timespec b = timespec_now();
+
+    timespec c =b-a;
+
+    double temps_exec = timespec_to_ms(c);*/
+    
+    
+    for (int i=0; i<3; ++i) 
+    {
+        pthread_join(incrementThread[i], nullptr);
+    }
+    pthread_mutex_destroy(&data.mutex);
+    std::cout << "Valeur finale du counter : " << data.counter << std::endl;
+    //std::cout << "temps d'exécution : " << temps_exec<< " ms" << std::endl;
 
     return 0 ;
 }
